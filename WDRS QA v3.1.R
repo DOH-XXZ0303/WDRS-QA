368a60c0ca0298c40e4bf27ae19c5b8b1a306365
Faustine Tay <142548681+DOH-FXT2303@users.noreply.github.com>
2023-08-22 15:38:21 -0700

    Update WDRS QA v3.1.R
    added case classification rule for "Not Reportable"

--- WDRS QA v3.1.R
+++ WDRS QA v3.1.R
@@ -94,6 +94,7 @@ qa_report<- qa_report %>% mutate(case_class= case_when((POSITIVE_PCR_LAB_DATE_CO
                                                        ~ "Incorrect case classification |",
                                                        is.na(DOH_CASE_CLASSIFICATION_GENERAL) ~ "missing case classification |",
                                                        DOH_CASE_CLASSIFICATION_GENERAL == 'Classification pending' ~ "Update case classification |"
+                                                       | DOH_CASE_CLASSIFICATION_GENERAL == 'Not reportable' ~ "Update case classification |"
                                                              ))
 
 
